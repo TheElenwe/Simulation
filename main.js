c9533f2ed5213a0d8b9747c0ef4a2f59a4b1e5be
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-02-14 17:13:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
-daynight=2
-step=2
+let daynight = 0;
+let count = 2;
 
-let counter=setInterval(function(){
-    
-   if (daynight<0 || daynight>12){
-    if (daynight >=step){
-   
-    step *=-1
-    daynight +=step    
-  
-     console.log(`${daynight}\u00b0 `)
-    }
-  
+let counter = setInterval(function () {
+  daynight += count;
+  if (daynight === 0 || daynight === 24) {
+    count = -count;
   }
-
-  else if (daynight<8){
-    console.log(`${daynight}\u00b0 on`)
+  console.log(daynight);
+  if (daynight <= 8) {
+    console.log(`${daynight}\u00b0 on`);
+  } else if (daynight >= 16) {
+    console.log(`${daynight}\u00b0 of`);
   }
-  else if (daynight>=8){
-    console.log(`${daynight}\u00b0 off`)
-  }
-
-   daynight+=step
-
-},500)
+}, 1000);
